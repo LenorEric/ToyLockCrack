a71ea1b774bd46fd90efedeee000ea794e5d1f84
LenorEric <40317566+LenorEric@users.noreply.github.com>
2022-06-26 06:09:29 +0800

    perfect solution

--- lock_with_swap.py
+++ lock_with_swap.py
@@ -8,9 +8,9 @@ def encrypt(a, b, c, const):
 def lock_with_swap(target, check):
     ret = ""
     pro_len = check % 3 + 1
+    remain = len(target)
     curr = 0
-    target += "00000"
-    while curr < len(target) - 5:
+    while remain:
         if pro_len == 1:
             ret += encrypt(curr, target[curr], check, 0)
         elif pro_len == 2:
@@ -21,10 +21,13 @@ def lock_with_swap(target, check):
             ret += encrypt(target[curr], curr, check, 6)
             ret += encrypt(target[curr + 1], curr, - check, 9)
         curr += pro_len
+        remain -= pro_len
         pro_len -= 1
         if pro_len == 0:
             pro_len = 3
-    return ret[:len(target) - 5]
+        if pro_len > remain:
+            pro_len = remain
+    return ret
 
 
 def add_check(target, check):
